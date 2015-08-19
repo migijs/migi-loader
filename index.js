430a8e0dffc969319c3d62f52e2815862586457b
army8735 <army8735@qq.com>
2015-08-19 10:50:50 +0800

    引入jaw

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var lefty = require('lefty');
+var jaw = require('jaw');
 
 var url = require('url');
-var path = require('path');
 
 module.exports = function(source) {
   if(this.cacheable) {
@@ -10,5 +10,16 @@ module.exports = function(source) {
   var config = url.parse(this.query, true).query;
   var lie = config.lie == 'true';
   var es5 = config.es5 == 'true';
+  var css = config.css == 'true';
+  var j = config.jaw == 'true';
+
+  if(css) {
+    return jaw.parse(source);
+  }
+
+  if(j) {
+    source = source.replace(/\bjaw\s*\.\s*parse\s*\(\s*(.+?)\s*\)/g, '$1');
+  }
+
   return lefty.parse(source, lie, es5);
 };
