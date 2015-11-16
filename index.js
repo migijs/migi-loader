6d05be064577760d0e5b4c7f7074169d68df6bc7
army8735 <army8735@qq.com>
2015-11-16 19:51:49 +0800

    remove jaw

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var lefty = require('lefty');
-var jaw = require('jaw');
 
 var url = require('url');
 
@@ -10,16 +9,6 @@ module.exports = function(source) {
   var config = url.parse(this.query, true).query;
   var lie = config.lie == 'true';
   var es5 = config.es5 == 'true';
-  var css = config.css == 'true';
-  var j = config.jaw == 'true';
-
-  if(css) {
-    return jaw.parse(source);
-  }
-
-  if(j) {
-    source = source.replace(/\bjaw\s*\.\s*parse\s*\(\s*(.+?)\s*\)/g, '$1');
-  }
 
   return lefty.parse(source, lie, es5);
 };
